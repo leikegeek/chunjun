90cdfe901b64454129943c2fac21fa0c5781eaea
zhumang <leikegeek@gmail.com>
2021-12-12 22:40:02 +0800

    1212

--- lab/decorators_demo.py
+++ lab/decorators_demo.py
@@ -6,4 +6,29 @@
 # @File : decorators_demo.py
 # @desc : 装饰器（Decorators）是修改其他函数的功能的函数
 
+from functools import wraps
+from urllib import request
 
+
+def log_handler(func):
+    @wraps(func)
+    def with_logging(*args, **kwargs):
+        print('Start %s(%s, %s)...' % (func.__name__, args, kwargs))
+        result = func(*args, **kwargs)
+        print("return message is " + str(result))
+        return result
+    return with_logging
+
+
+@log_handler
+def add_func(x):
+    return x + x
+
+
+@log_handler
+def remove_func(name=None, age=1):
+    print("name is" + name + ", age is " + str(age))
+
+
+s1 = add_func(4)
+remove_func("tom", 5)
